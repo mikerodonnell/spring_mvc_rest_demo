81d48771fff95bfbe356dc43edc315fd70ee46f0
Michael ODonnell <modonnell@grassroots-tech.com>
2015-10-10 20:44:49 +0000

    adding validation of response body to restassured tests.

--- src/test/java/com/demo/recipe/controller/RecipeControllerIT.java
+++ src/test/java/com/demo/recipe/controller/RecipeControllerIT.java
@@ -4,11 +4,13 @@ import static com.demo.recipe.RecipeTestData.EXTANT_RECIPE_UUID;
 import static com.demo.recipe.RecipeTestData.NONEXISTANT_RECIPE_UUID;
 import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
+import static com.demo.recipe.dao.stub.StubRecipeDao.*;
 import com.demo.recipe.serialization.RecipeRepresentation;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
@@ -25,16 +27,29 @@ public class RecipeControllerIT {
 	
 	@Test
 	public void testGetRecipe() {
-		get("recipe/" + EXTANT_RECIPE_UUID).then().assertThat().statusCode(HttpStatus.OK.value());
-		get("recipe/" + NONEXISTANT_RECIPE_UUID).then().assertThat().statusCode(HttpStatus.BAD_REQUEST.value());
+		get("recipe/" + RECIPE_1.getUuid()).then().assertThat().
+			statusCode(HttpStatus.OK.value()).
+			contentType(ContentType.JSON).
+			body( "uuid", equalTo(RECIPE_1.getUuid()) ).
+			body( "recipeName", equalTo(RECIPE_1.getRecipeName()) );
+		
+		get("recipe/" + NONEXISTANT_RECIPE_UUID).then().assertThat().
+			statusCode(HttpStatus.BAD_REQUEST.value());
 	}
 	
 	@Test
 	public void testSearchRecipesByIngredientName() {
-		get("recipe/search/ham").then().assertThat().contentType(ContentType.JSON).statusCode(HttpStatus.OK.value());
+		get("recipe/search/ham").then().assertThat().
+			statusCode(HttpStatus.OK.value()).
+			contentType(ContentType.JSON).
+			body( "$", hasSize(1) ). // $ is the placeholder for an anonymous JSON array at the root of JSON. assert that the response array has size 1 (1 recipe contains ham).
+			body( "uuid", hasItems(RECIPE_1.getUuid()) ); // assert that the response array contains the UUID of the green eggs and ham recipe
 		
 		// verify that searching for an ingredient for which there are no recipes still returns HTTP 200, and empty result set.
-		get("recipe/search/salsa").then().assertThat().contentType(ContentType.JSON).statusCode(HttpStatus.OK.value());
+		get("recipe/search/salsa").then().assertThat().
+			statusCode(HttpStatus.OK.value()).
+			contentType(ContentType.JSON).
+			body( "$", empty() ); // valid response, but no results. empty array.
 	}
 	
 	@Test
@@ -47,7 +62,11 @@ public class RecipeControllerIT {
 		requestBody.setAmount1(1);
 		requestBody.setAmountUnit1("cup");
 		
-		given().contentType(ContentType.JSON).body(requestBody).put("recipe/create").then().assertThat().statusCode(HttpStatus.OK.value());
+		given().contentType(ContentType.JSON).body(requestBody).put("recipe/create").then().assertThat().
+			statusCode(HttpStatus.OK.value()).
+			contentType(ContentType.JSON).
+			body( "uuid", not(isEmptyOrNullString()) ).
+			body( "recipeName", equalTo("irish bread") );
 	}
 	
 	@Test
@@ -56,13 +75,15 @@ public class RecipeControllerIT {
 		RecipeRepresentation requestBody = new RecipeRepresentation();
 		requestBody.setRecipeName("irish bread");
 		
-		given().contentType(ContentType.JSON).body(requestBody).put("recipe/create").then().assertThat().statusCode(HttpStatus.BAD_REQUEST.value());
+		given().contentType(ContentType.JSON).body(requestBody).put("recipe/create").then().assertThat().
+			statusCode(HttpStatus.BAD_REQUEST.value());
 	}
 	
 	@Test
 	public void testCreateNullBody() {
 		// providing no post data at all ultimately triggers a RecipeCreationException for no recipe name, which results in HTTP 400
-		given().contentType(ContentType.JSON).put("recipe/create").then().assertThat().statusCode(HttpStatus.BAD_REQUEST.value());
+		given().contentType(ContentType.JSON).put("recipe/create").then().assertThat().
+			statusCode(HttpStatus.BAD_REQUEST.value());
 	}
 	
 	@Test
@@ -70,17 +91,20 @@ public class RecipeControllerIT {
 		RecipeRepresentation requestBody = new RecipeRepresentation();
 		requestBody.setPrepMinutes(99);
 		
-		given().contentType(ContentType.JSON).body(requestBody).put("recipe/update/" + EXTANT_RECIPE_UUID).then().assertThat().statusCode(HttpStatus.OK.value());
+		given().contentType(ContentType.JSON).body(requestBody).put("recipe/update/" + EXTANT_RECIPE_UUID).then().assertThat().
+			statusCode(HttpStatus.OK.value());
 	}
 	
 	@Test
 	public void testUpdateNullBody() {
 		// providing a valid recipe id to update, but no details of what to update at all, should result in HTTP 400
-		given().contentType(ContentType.JSON).put("recipe/update/" + EXTANT_RECIPE_UUID).then().assertThat().statusCode(HttpStatus.BAD_REQUEST.value());
+		given().contentType(ContentType.JSON).put("recipe/update/" + EXTANT_RECIPE_UUID).then().assertThat().
+			statusCode(HttpStatus.BAD_REQUEST.value());
 	}
 	
 	@Test
 	public void testUpdateInvalidId() {
-		given().contentType(ContentType.JSON).put("recipe/update/" + NONEXISTANT_RECIPE_UUID).then().assertThat().statusCode(HttpStatus.BAD_REQUEST.value());
+		given().contentType(ContentType.JSON).put("recipe/update/" + NONEXISTANT_RECIPE_UUID).then().assertThat().
+			statusCode(HttpStatus.BAD_REQUEST.value());
 	}
 }
